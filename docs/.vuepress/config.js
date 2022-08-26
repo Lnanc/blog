b14fa72a48e9e5e5425a7298c0aac4d06069a295
yanglihao@tuzhanai.com <yangcheng@tuzhanai.com>
2022-08-26 18:21:38 +0800

    "初始化"

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  // title: 'Hello VuePress',
+  // description: 'Just playing around'
+  themeConfig: {
+    lastUpdated: "更新时间",
+    // navbar: false,
+    sidebar: "auto",
+    // sidebar: ["about","guide/"],
+    // sidebar: [
+    //   {
+    //     title: 'Group 1',   // 必要的
+    //     path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+    //     collapsable: false, // 可折叠的 可选的, 默认值是 true,
+    //     sidebarDepth: 1,    // 可选的, 默认值是 1
+    //     children: [
+    //       '/css/c-aaa',
+    //       '/css/c-bbb',
+    //       '/css/c-ccc',
+    //     ]
+    //   },
+    //   {
+    //     title: 'Group 2',
+    //     children: [ /* ... */ ],
+    //     initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+    //   }
+    // ],
+    // sidebar: {
+    //   "/css/": ["c-aaa", "c-bbb", "c-ccc", "/"],
+    // },
+    logo: "/assets/img/logo.png",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "About", link: "/about" },
+      {
+        text: "Languages",
+        items: [
+          {
+            text: "Group1",
+            items: [
+              { text: "Home", link: "/" },
+              { text: "Guide", link: "/guide/" },
+            ],
+          },
+          {
+            text: "Group2",
+            items: [
+              { text: "Home", link: "/" },
+              { text: "Guide", link: "/guide/" },
+            ],
+          },
+        ],
+      },
+      { text: "External", link: "https://google.com" },
+    ],
+  },
+};
